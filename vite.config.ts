5c28585a1678b2ac178c3996c650de08c8121d65
oluseunakin <akindoyinoluseun@gmail.com>
2024-09-08 10:09:41 +0100

    another try

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
   plugins: [
@@ -11,15 +10,13 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
-      serverModuleFormat: "cjs",
+      serverModuleFormat: "esm",
     }),
     tsconfigPaths(),
-    viteCommonjs(),
   ],
   optimizeDeps: {
     esbuildOptions: {
       target: "esnext",
     },
-    include: ["@mui/material", "@mui/icons-material", "@mui/utils"], // Add specific dependencies if needed
   },
 });
