a347bfdf01be54e1b777962844f187d1585cadc1
oluseunakin <akindoyinoluseun@gmail.com>
2024-09-08 10:16:56 +0100

    hjhjh

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,18 +1,22 @@
-import * as ReactDOMServer from 'react-dom/server';
-import { RemixServer } from '@remix-run/react';
-import type { EntryContext } from '@remix-run/node';
-import createEmotionCache from './createEmotionCache';
-import theme from './theme';
-import {CssBaseline} from '@mui/material/';
-import { ThemeProvider } from '@mui/material/styles';
-import { CacheProvider } from '@emotion/react';
-import createEmotionServer from '@emotion/server/create-instance';
+import * as ReactDOMServer from "react-dom/server";
+import { RemixServer } from "@remix-run/react";
+import type { EntryContext } from "@remix-run/node";
+import createEmotionCache from "./createEmotionCache";
+import theme from "./theme";
+import materialPkg from "@mui/material/";
+import stylePkg from "@mui/material/styles";
+import reactPkg from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
+
+const { CssBaseline } = materialPkg;
+const { ThemeProvider } = stylePkg;
+const { CacheProvider } = reactPkg;
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
+  remixContext: EntryContext
 ) {
   const cache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(cache);
@@ -35,10 +39,10 @@ export default function handleRequest(
   // Grab the CSS from emotion
   const { styles } = extractCriticalToChunks(html);
 
-  let stylesHTML = '';
+  let stylesHTML = "";
 
   styles.forEach(({ key, ids, css }) => {
-    const emotionKey = `${key} ${ids.join(' ')}`;
+    const emotionKey = `${key} ${ids.join(" ")}`;
     const newStyleTag = `<style data-emotion="${emotionKey}">${css}</style>`;
     stylesHTML = `${stylesHTML}${newStyleTag}`;
   });
@@ -46,10 +50,10 @@ export default function handleRequest(
   // Add the Emotion style tags after the insertion point meta tag
   const markup = html.replace(
     /<meta(\s)*name="emotion-insertion-point"(\s)*content="emotion-insertion-point"(\s)*\/>/,
-    `<meta name="emotion-insertion-point" content="emotion-insertion-point"/>${stylesHTML}`,
+    `<meta name="emotion-insertion-point" content="emotion-insertion-point"/>${stylesHTML}`
   );
 
-  responseHeaders.set('Content-Type', 'text/html');
+  responseHeaders.set("Content-Type", "text/html");
 
   return new Response(`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
